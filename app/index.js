292f15002ec5660f6726c395a96a5b3109d27c10
ElBobo <luckybobo@ymail.com>
2017-09-02 11:19:19 +0200

    database connection settings

--- app/index.js
+++ app/index.js
@@ -3,8 +3,15 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql      = require('mysql');
 
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config')[env];
+
 const connection = mysql.createConnection({
-  port: 3308
+  host     : config.database.host,
+  user     : config.database.user,
+  password : config.database.password,
+  database : config.database.database,
+  port: config.database.port
 });
 
 const app = express()
@@ -31,7 +38,7 @@ app.get('/', (request, response) => {
   })
 })
 
-var employee = { name: 'pankaj', passhash: 'india' };
+var employee = { name: 'pankaj2', passhash: 'india' };
 connection.query('INSERT INTO user SET ?', employee, function(err,res){
   if(err) throw err;
 
