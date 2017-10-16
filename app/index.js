cc47bebe5f183ffbb0f73c5cf08946eeddb73ef6
Tobias <lobitzto@msn.com>
2017-10-16 11:22:32 +0200

    für den hacktober

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-const path = require('path')  
-const express = require('express')  
+const path = require('path')
+const express = require('express')
 const exphbs = require('express-handlebars')
 const mysql      = require('mysql');
 
@@ -18,21 +18,21 @@ const app = express()
 
 connection.connect(function(err){
 if(!err) {
-    console.log("Database is connected ... nn");    
+    console.log("Database is connected ... nn");
 } else {
-    console.log("Error connecting database ... nn");    
+    console.log("Error connecting database ... nn");
 }
 });
 
-app.engine('.hbs', exphbs({  
+app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, '../views/layouts')
 }))
-app.set('view engine', '.hbs')  
-app.set('views', path.join(__dirname, '../views'))  
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, '../views'))
 
-app.get('/', (request, response) => {  
+app.get('/', (request, response) => {
   response.render('home', {
     name: 'John'
   })
@@ -43,6 +43,9 @@ connection.query('INSERT INTO user SET ?', employee, function(err,res){
   if(err) throw err;
 
   console.log('Last insert ID:', res.insertId);
+
+  // muss hier nur was anfügen für den Hackathon 
+
 });
 
 app.listen(3000)
